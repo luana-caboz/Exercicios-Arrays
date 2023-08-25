f44309ec8310e1c7535b18e58dd7f8823279c5c3
luana-caboz <luanacaboz31@hotmail.com>
2023-08-24 21:41:14 -0300

    Execucao Exercicio1

--- Exercicio1.java
+++ Exercicio1.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Exercicio1 {
 
-    int nota;
+    public static int[] criarVetor(int tamanho){
+        int[] notas = new int[tamanho];
+        Scanner teclado = new Scanner(System.in);
+        for(int i = 0; i < 10; i++){
+            System.out.printf("Vetor[%d]: \n", i);
+            notas[i] = teclado.nextInt();
+        }
+        return notas;
+    }
 
-    System.out.printl("A nota é " +nota);
+    public static void imprimeNotas(int[] notas){
+            System.out.println("A nota de índice 7 é "+notas[7]);
+            System.out.println("A nota de índice 9 é " +notas[9]);
+    }
+
+    public static void main(String[] args) {
+
+        int tamanho = 10;
+        
+        int[] notas = criarVetor(tamanho);
+
+        imprimeNotas(notas);
+        
+    }
+
+}
     
